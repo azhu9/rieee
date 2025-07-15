ad7764675cfee243ce8a3f78374017d96fec6882
azhu9 <109678994+azhu9@users.noreply.github.com>
2025-07-15 16:25:04 -0400

    maps embed

--- src/components/DivBody.jsx
+++ src/components/DivBody.jsx
@@ -53,6 +53,17 @@ const DivBody = ({ title, carouselSlides, description, meetingTime, meetingLocat
         </p>
 
       </section>
+      <div className="max-w-5xl h-[400px] m-4 md:mx-auto">
+        <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3032.8986639100144!2d-74.46328912254491!3d40.521730871422875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3c70c51296859%3A0x62d02d4fb7e7e2bf!2sElectrical%20Engineering%20Building%2C%20Piscataway%2C%20NJ%2008854!5e0!3m2!1sen!2sus!4v1752610406432!5m2!1sen!2sus"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     <Footer/>
     </div>
   );
